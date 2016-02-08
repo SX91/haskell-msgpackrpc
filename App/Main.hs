8fe0f71eaa4044190de8c704f478caad640cc9fc
Anton K <sx91@protonmail.com>
2016-02-08 20:23:00 +0300

    Refactor example

--- App/Main.hs
+++ App/Main.hs
@@ -2,22 +2,30 @@
 {-# LANGUAGE OverloadedStrings #-}
 import           BasicPrelude              hiding (catch, finally, try)
 
--- import           Control.Monad.Catch
+import           Control.Monad.Catch
 import qualified Data.Map                  as Map
 
 import           Network.MsgpackRpc.Server
 
-import           Debug.Trace
 
 default (Text)
 
 
-testMethod :: Int -> Int -> RpcMethodT IO Int
-testMethod a b = return $! a + b
+sumMethod :: Int -> Int -> RpcMethodT IO Int
+sumMethod a b = return $! a + b
+
+errorMethod :: RpcMethodT IO Int
+errorMethod = throwM $ rpcError ".MyRpc.SomeCoolError" ("WAT!?" :: Text)
+
+func :: IO ()
+func = do
+    let methodMap =  Map.fromList
+            [
+              ("sum", rpcMethod sumMethod)
+            , ("error", rpcMethod errorMethod)
+            ]
+    serveRpc 128 (serverSettings 5252 "*") methodMap
 
 main :: IO ()
-main = do
-    traceIO "---->"
-    let methodMap =  Map.fromList [("sum", rpcMethod testMethod)]
-    serveRpc 128 (serverSettings 5252 "*") methodMap
+main = func
 
